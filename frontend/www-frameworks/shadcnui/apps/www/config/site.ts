0a4fd88f01cbd8069f0fdf848e12a0c8791e1a9d
Being Of Existence <beingofexistence03@outlook.com>
2024-01-11 13:21:16 +0000

    feat: old dx

--- frontend/www-frameworks/shadcnui/apps/www/config/site.ts
+++ frontend/www-frameworks/shadcnui/apps/www/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: "https://ui.shadcn.com",
   ogImage: "https://ui.shadcn.com/og.jpg",
   description:
-    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    "Develop Previe Ship your software with ease!!!",
   links: {
     twitter: "https://twitter.com/shadcn",
     github: "https://github.com/shadcn-ui/ui",
