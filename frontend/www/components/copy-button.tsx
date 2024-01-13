f3029f9d219d26f34b035c09f70c114247306282
Being Of Existence <beingofexistence03@outlook.com>
2024-01-13 17:36:40 +0000

    feat: vercel deployment

--- frontend/www/components/copy-button.tsx
+++ frontend/www/components/copy-button.tsx
@@ -7,13 +7,13 @@ import { NpmCommands } from "../types/unist"
 
 import { Event, trackEvent } from "@/lib/events"
 import { cn } from "@/lib/utils"
-import { Button } from "@/registry/new-york/ui/button"
+import { Button } from "./ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/registry/new-york/ui/dropdown-menu"
+} from "./ui/dropdown-menu"
 
 interface CopyButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   value: string
