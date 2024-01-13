f29add112e3169ed4210dd6f86483a9503fa0cae
Being Of Existence <beingofexistence03@outlook.com>
2024-01-13 10:30:39 +0000

    feat: vercel deployment

--- frontend/www/app/page.tsx
+++ frontend/www/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           <AccordionItem value="item-3">
             <AccordionTrigger>Is it animated?</AccordionTrigger>
             <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you prefer.
+              Yes. It is animated by default, but you can disable it if you prefer.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
